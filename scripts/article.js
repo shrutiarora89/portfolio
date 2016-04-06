1471f03589d985bdc0f2e6db555062409e29b843
Shruti Arora <shruti.arora89@icloud.com>
2016-04-05 18:45:04 -0700

    Still need work on IIFE

--- scripts/article.js
+++ scripts/article.js
@@ -1,96 +1,100 @@
-// array for projects
- articlesProjects = [];
+var myself = this;
 
-// array for education
- articlesEducation = [];
+(function(module) {
 
- function Article (opts) {
-   this.title = opts.title;
-   this.institute = opts.institute;
-   this.instituteUrl = opts.instituteUrl;
-   this.category = opts.category;
-   this.body = opts.body;
- }
+  function Article (opts) {
+    myself.title = opts.title;
+    myself.institute = opts.institute;
+    myself.instituteUrl = opts.instituteUrl;
+    myself.category = opts.category;
+    myself.body = opts.body;
+  }
 
- Article.prototype.toHtml = function() {
+  // array for projects
+  articlesProjects = [];
 
-   var theTemplateScript = $('#rawData-template').html();
-   var theTemplate = Handlebars.compile(theTemplateScript);
+  // array for education
+  articlesEducation = [];
 
-   var theCompiledHtml = theTemplate(this);
-   return theCompiledHtml;
- };
+  Article.prototype.toHtml = function() {
+    var theTemplateScript = $('#rawData-template').html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    var theCompiledHtml = theTemplate(myself);
+    return theCompiledHtml;
+  };
 
-
+// Refactoring the forEach code by using .map method
 // Function that pushes the data into the array articlesEducation and insantiating a new object Education using Constructor.
- Article.loadEducation = function(data){
-   data.forEach(function(ele) {
-     articlesEducation.push(new Article(ele));
-   });
-
- };
+  Article.loadEducation = function(data){
+    data.map(function(ele) {
+      console.log(data);
+      articlesEducation.push(new Article(ele));
+    });
+  };
 
  // Function that pushes the data into the array articlesProjects and insantiating a new object Project using Constructor.
- Article.loadProjects = function(data){
-   data.forEach(function(ele) {
-     articlesProjects.push(new Article(ele));
-   });
+  Article.loadProjects = function(data){
+    data.map(function(ele) {
+      console.log(data);
+      articlesProjects.push(new Article(ele));
+    });
 
- };
+  };
 
  // ------------------------fetchEducation--------------------------------------------------------
 
- Article.fetchEducation = function(){
+  Article.fetchEducation = function(){
    //When the rawDataEducation is already in localStorage
   //  Second time- It will get the item by the key rawDataEducation which is set on line 61.
-   if (localStorage.getItem('rawDataEducation')){
-     var data = JSON.parse(localStorage.getItem('rawDataEducation'));
+    if (localStorage.getItem('rawDataEducation')){
+      var data = JSON.parse(localStorage.getItem('rawDataEducation'));
      //load all the data using .loadAll function
-     Article.loadEducation(data);
+      Article.loadEducation(data);
      //render the index page
-     articleView.initIndexPage();
-   }
+      articleView.initIndexPage();
+    }
   else { //When we don't have rawDataEducation in the  localStorage
      //retriving the JSON file from the server with AJAX getJSON method
-     $.getJSON('data/rawDataEducation.json',function(data){
+      $.getJSON('data/rawDataEducation.json',function(data){
        //load all the data using .loadAll function
-       Article.loadEducation(data);
+        Article.loadEducation(data);
        //caching it in the localStorage (Key-rawDataEducation, value-JSON.stringfy(data))
        //First Time- Setting the key for the localStorage.So that we can get it with the Key 'rawDataEducation'.
-       localStorage.setItem('rawDataEducation',JSON.stringify(data));
+        localStorage.setItem('rawDataEducation',JSON.stringify(data));
        //render the index page
-       articleView.initIndexPage();
-     });
-   }
- };
+        articleView.initIndexPage();
+      });
+    }
+  };
  // -------------------------------------------------------------------------------------------------
 
 
 
 // ------------------------fetchProjects--------------------------------------------------------
- Article.fetchProjects = function(){
+  Article.fetchProjects = function(){
    //When the rawDataEducation is alredy in localStorage
   //  Second time- It will get the item by the key rawDataProjects which is set on line 80.
   // The localStorage will have the data
-   if (false && localStorage.getItem('rawDataProjects')){
+    if (localStorage.getItem('rawDataProjects')){
      //load all the data using .loadAll function
-     var data = JSON.parse(localStorage.getItem('rawDataProjects'));
-     Article.loadProjects(data);
+      var data = JSON.parse(localStorage.getItem('rawDataProjects'));
+      Article.loadProjects(data);
      //render the index page
-     articleView.initIndexPage();
-   }
+      articleView.initIndexPage();
+    }
   else { //When we don't have rawDataProjects in the  localStorage
      //retriving the JSON file from the server with AJAX getJSON method
-     $.getJSON('data/rawDataProjects.json',function(data){
-       console.log(data);
+      $.getJSON('data/rawDataProjects.json',function(data){
+        console.log(data);
        //load all the data using .loadAll function
-       Article.loadProjects(data);
+        Article.loadProjects(data);
        //caching it in the localStorage (Key-rawDataEducation, value-JSON.stringfy(data))
        //First Time- Setting the key for the localStorage.So that we can get it with the Key 'rawDataEducation'.
-       localStorage.setItem('rawDataProjects',JSON.stringify(data));
+        localStorage.setItem('rawDataProjects',JSON.stringify(data));
        //render the index page
-       articleView.initIndexPage();
-     });
-   }
- };
+        articleView.initIndexPage();
+      });
+    }
+  };
  // -------------------------------------------------------------------------------------------------
+})(window);
